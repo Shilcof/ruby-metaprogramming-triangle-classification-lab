58397117e3e54739ee4ef960e913a7bbb89395be
Shilcof <robert.shilcof@gmail.com>
2020-10-28 19:13:47 +0000

    commit tree c6fa8415ba6145346d0612a638e4fd4b9abe1f56

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    self.a = a
+    self.b = b
+    self.c = c
+  end
+
+  def kind
+    if [a, b, c].sort[2] >= [a, b, c].sort[1] + [a, b, c].sort[0] || [a, b, c].any?{|i| i <=0}
+      raise TriangleError
+    elsif [a, b, c].uniq.size == 1
+      :equilateral
+    elsif [a, b, c].uniq.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
